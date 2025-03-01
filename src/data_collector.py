89ab13824d301b3d32c72ff0129be80535fdbc23
ChrissElhelou <christofer.el-helou@hotmail.com>
2025-03-01 20:23:21 +0100

    Update data_collector.py

--- src/data_collector.py
+++ src/data_collector.py
@@ -4,11 +4,9 @@ import pandas as pd
 
 class YahooFinanceFetcher:
     """Fetches historical market data from Yahoo Finance using yfinance."""
-    
     def get_prices(self, tickers, start_date=None, end_date=None):
         """Fetches historical market data for given tickers and dates."""
         try:
-            # Set auto_adjust=False to retrieve the raw "Adj Close" column
             data = yf.download(tickers, start=start_date, end=end_date, progress=False, auto_adjust=False)
             df = data['Adj Close']
         except Exception as e:
